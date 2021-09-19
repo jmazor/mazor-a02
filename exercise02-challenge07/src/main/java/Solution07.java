a4d1995d57edf007e6ced315149deb7ac7fc42ba
jmazor <jmazor@users.noreply.github.com>
2021-09-19 19:52:25 -0400

    Finished 07

--- exercise02-challenge07/src/main/java/Solution07.java
+++ exercise02-challenge07/src/main/java/Solution07.java
@@ -1,7 +1,14 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class Solution07 {
     public static void main(String[] args) {
-
+        System.out.print("What is the length of the room in feet? ");
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        System.out.print("What is the width of the room in feet? ");
+        int width = sc.nextInt();
+        System.out.println("You entered the dimensions of " + length + " feet by " + width + " feet.");
+        int f2 = width * length;
+        System.out.println("The area is \n" + f2 + " square feet\n" + (f2*0.09290304) + " square meters");
     }
 }
