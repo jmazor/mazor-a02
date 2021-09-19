c701d0346ba07e8019ac52a16b9f9475dd234d6d
jmazor <jmazor@users.noreply.github.com>
2021-09-19 19:40:49 -0400

    Finished 06

---
+++ exercise02-challenge06/src/main/java/Solution6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.time.Year;
+
+public class Solution6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is your current age? ");
+        int age = sc.nextInt();
+
+        System.out.print("At what age would you like to retire? ");
+        int retire = sc.nextInt();
+
+        System.out.println("You have " + (retire-age) + " years left until you can retire.");
+        int year = Year.now().getValue();
+        System.out.print("It's " + year + ", so you can retire in " + (year+(retire-age)) + ".");
+    }
+}
