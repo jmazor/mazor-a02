e9b1a4cfa6b0afdb7579c43136d927e156ae2c0f
jmazor <jmazor@users.noreply.github.com>
2021-09-19 20:52:44 -0400

    Solution10

---
+++ exercise02-challenge10/src/main/java/Solution10.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 10 Solution
+ *  Copyright 2021 Justin Mazor
+ */
+
+import java.util.Scanner;
+import java.util.Formatter;
+
+public class Solution10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the price of item 1: ");
+        double price1 = sc.nextDouble();
+        System.out.print("Enter the quantity of item 1: ");
+        double q1 = sc.nextInt();
+
+        System.out.print("Enter the price of item 2: ");
+        double price2 = sc.nextDouble();
+        System.out.print("Enter the quantity of item 2: ");
+        double q2 = sc.nextInt();
+
+        System.out.print("Enter the price of item 3: ");
+        double price3 = sc.nextDouble();
+        System.out.print("Enter the quantity of item 3: ");
+        double q3 = sc.nextInt();
+
+        double sub = (price1 * q1) + (price2 * q2) + (price3 * q3);
+        double tax = sub * 0.055;
+        double total = tax + sub;
+        System.out.printf("Subtotal: $%.2f%n", sub);
+        System.out.printf("Tax: $%.2f%n", tax);
+        System.out.printf("Total: $%.2f%n", total);
+
+    }
+}
