a18871c80a6d2cb9508b714c77d94395a4ccb537
jmazor <jmazor@users.noreply.github.com>
2021-09-19 20:32:56 -0400

    Solution 09

---
+++ exercise02-challenge09/src/main/java/Solution09.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 09 Solution
+ *  Copyright 2021 Justin Mazor
+ */
+import java.util.Scanner;
+
+public class Solution09 {
+    public static void main(String[] args) {
+        System.out.print("What is the length of the room in feet? ");
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        System.out.print("What is the width of the room in feet? ");
+        int width = sc.nextInt();
+        int f2 = length * width;
+        int gallons;
+        if (f2%350 != 0) {
+            gallons = (f2/350) + 1;
+        } else
+            gallons = f2/350;
+        System.out.println("You will need to purchase " + gallons + " gallons of point to cover " + f2 + " square feet.");
+
+    }
+}
